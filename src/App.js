09fc8fa564f91b1ac119a2b60eed641be9411e70
RenjiraND <13516014@std.stei.itb.ac.id>
2019-04-07 20:20:01 +0700

    added  add-rfid function for admin

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.ref = fire.ref('users');
+    this.ref_rfid = fire.ref('rfid');
     this.state = {
         email:'',
+        rfid:'',
         validEmails : []      
     };
 }
-
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          
           <div class="form-group container card text-dark p-5">
             <label htmlFor="email" className="text-left">Input E-mail:</label>
             <hr/>
@@ -26,10 +28,24 @@ class App extends Component {
               className="form-control my-2" 
               name="email"
               value={this.state.email}
-              placeholder="input valid gmail here"
+              placeholder="input a valid gmail here"
               onChange={this.handleChange} />
             <button type="button" class="btn btn-primary" onClick={this.handleClick}>Add User</button>
           </div>
+
+          <div class="form-group container card text-dark p-5">
+            <label htmlFor="rfid" className="text-left">Input RFID-Tag</label>
+            <hr/>
+            <input 
+              type="rfid" 
+              className="form-control my-2" 
+              name="rfid"
+              value={this.state.rfid}u
+              placeholder="input a valid RFID-Tag here"
+              onChange={this.handleChange} />
+            <button type="button" class="btn btn-primary" onClick={this.handleClickRFID}>Add RFID-Tag</button>
+          </div>
+
         </header>
       </div>
     );
@@ -41,6 +57,14 @@ class App extends Component {
       email:this.state.email
     }
     this.ref.push(user);
+  }
+
+  handleClickRFID = (e) => {
+    e.preventDefault();
+    const rfid_tag = {
+      rfid:this.state.rfid
+    }
+    this.ref_rfid.push(rfid_tag);
   }
 
   handleChange = (e) => {
